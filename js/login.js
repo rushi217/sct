d86c2343ac9b1f82515dac427d9510be17f4915f
rushi217 <rushi21.d@gmail.com>
2018-03-19 02:11:07 +0530

    new file:   assets/css/animate.css 	new file:   assets/css/icomoon.css 	new file:   assets/css/simple-line-icons.css 	new file:   css/icomoon.css 	new file:   css/login.css 	modified:   css/main.css 	modified:   css/main1.css 	new file:   css/simple-line-icons.css 	new file:   css/style.css 	new file:   homepage.html 	modified:   index.html 	new file:   js/login.js 	new file:   js/modernizr-2.6.2.min.js 	modified:   learn.html 	deleted:    login.html 	new file:   login1.php

---
+++ js/login.js
@@ -0,0 +1,31 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var menu = document.getElementById('menu'),
+    panelMenu = menu.querySelectorAll('li'),
+    panelBoxes = document.querySelectorAll('.panel__box'),
+    signUp = document.getElementById('signUp'),
+    signIn = document.getElementById('signIn');
+
+function removeSelection(){
+    for(var i = 0, len = panelBoxes.length; i < len; i++){panelBoxes[i].classList.remove('active');
+    }
+}
+
+
+signIn.onclick = function(e){
+  e.preventDefault();
+  removeSelection();
+  panelBoxes[0].classList.add('active');
+  menu.classList.remove('second-box');
+}
+
+signUp.onclick = function(e){
+  e.preventDefault();
+  removeSelection();
+  panelBoxes[1].classList.add('active');
+  menu.classList.add('second-box');
+}
+
